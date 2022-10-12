2da0e4c90393600844920ee1792062158a880318
Anton <Iarovoi>
2022-10-12 08:20:59 +0300

    trully figured sandbox

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -17,23 +17,15 @@ const server = new ApolloServer({
 const startServer = server.start()
 
 export default async function handler(req: MicroRequest, res: ServerResponse<IncomingMessage>) {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "https://studio.apollographql.com");
     res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://studio.apollographql.com"
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
     );
-    // res.setHeader(
-    //     "Access-Control-Allow-Headers",
-    //     "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
-    // );
-    // res.setHeader(
-    //     "Access-Control-Allow-Methods",
-    //     "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
-    // );
-    // if (req.method === "OPTIONS") {
-    //     res.end();
-    //     return false;
-    // }
+    if (req.method === "OPTIONS") {
+        res.end();
+        return false;
+    }
 
     await startServer
     await server.createHandler({path: "/api/graphql", disableHealthCheck: true})(req, res)
