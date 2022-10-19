38dc1085c9fb28e774f7395391ea3cdcb66a6983
Пользователь <alaravoi@medialink.com.cy>
2022-10-19 20:51:44 +0300

    smallfix

--- components/ui/Spacer/Spacer.tsx
+++ components/ui/Spacer/Spacer.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import s from './Spacer.module.scss'
 
 const Spacer = ({height, backgroundColor}: {height: string, backgroundColor: string}) => {
-    return (
-        <div className={s.spacer} style={{height, backgroundColor}} />
-    );
+    return <div className={s.spacer} style={{height, backgroundColor}} />
 };
 
 export default Spacer;
