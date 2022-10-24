2965aa5a755786d7ae2a046b3d1507157be5f493
Пользователь <alaravoi@medialink.com.cy>
2022-10-24 21:15:24 +0300

    useSession in home page as an example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from 'next'
+import {useSession} from "next-auth/react";
 
 const Home: NextPage = () => {
+const session = useSession()
+    console.log(session)
+
   return (
     <div>
       HOMe
