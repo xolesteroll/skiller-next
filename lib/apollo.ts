ef5449a0b0562d3790b710b563c889cc2c27ff4d
Anton <Iarovoi>
2022-11-02 18:16:28 +0200

    changed base url to be taken from .env

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-    uri: "http://localhost:3000/api/graphql",
+    uri: process.env.BASE_URL,
     cache: new InMemoryCache()
 })
 
